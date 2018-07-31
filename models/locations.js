476638862f26f84312bc6d753269c0e450e94c60
Nicole Houston <nicole.houston@cox.net>
2018-07-31 14:54:58 -0700

    removed user model

--- models/locations.js
+++ models/locations.js
@@ -1,35 +1,31 @@
-module.exports = function(sequelize, DataTypes) {
-    var Locations = sequelize.define("Locations", {
-      locationName: {
-        type: DataTypes.STRING(100),
-        allowNull: true,
-        validate: {
-          len: [1]
-        }
-      },
-      latitude: {
-        type: DataTypes.DECIMAL(18,14),
-        allowNull: false
-      },
-      longitude: {
-        type: DataTypes.DECIMAL(18,14),
-        allowNull: false
-      },
-      radius: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    });
 
-    Locations.associate = function(models) {
+module.exports = function(sequelize, DataTypes) {
+  var Locations = sequelize.define("Locations", {
+    locationName: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    },
+    latitude: {
+      type: DataTypes.DECIMAL(18,14),
+      allowNull: false
+    },
+    longitude: {
+      type: DataTypes.DECIMAL(18,14),
+      allowNull: false
+    },
+    radius: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+
+  Locations.associate = function(models) {
       Locations.hasMany(models.People, {
         onDelete: "cascade"
       });
-      Locations.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: true
-        }
-      });
     };
-    return Locations;
+  return Locations;
 };
--- models/people.js
+++ models/people.js
@@ -1,44 +1,45 @@
 module.exports = function(sequelize, DataTypes) {
-    var People = sequelize.define("People", {
-      firstName: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      lastName: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      nickname: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      role: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      notes: {
-        type: DataTypes.TEXT,
-        allowNull: true
+  var People = sequelize.define("People", {
+    firstName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        len: [1]
       }
-    });
-    People.associate = function(models) {
+    },
+    lastName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    nickname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    role: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    notes: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  });
+
+  People.associate = function(models) {
       People.belongsTo(models.Locations, {
         foreignKey: {
           allowNull: false
         }
       });
     };
-    return People;
-  };
+  return People;
+};
